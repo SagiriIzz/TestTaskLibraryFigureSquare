a651048dfd2b1e5301e75a308e0ba0b01c11fdb6
Sagiri Izumi <romaty29@gmail.com>
2022-12-05 18:20:17 +0700

    add

---
+++ LibraryFigureAreaTests/CheckingTriangleIsRightTest.cs
@@ -0,0 +1,17 @@
+﻿using TestTaskLibraryFigureSquare;
+namespace FigureSquareTests;
+
+[TestClass]
+public class CheckingTriangleIsRightTest
+{
+    [TestMethod]
+    public void  Right_Triangle_ShouldReturnTrue()
+    {
+        var tringle = new Triangle(firstSide:7, secondSide:24, thirdSide:25);
+        const bool expected = true;
+        
+        var actual =  tringle.IsRight();
+        
+        Assert.AreEqual(expected, actual);
+    }
+}
